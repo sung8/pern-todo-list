35860eb509379923e70b0bac34172a55f051520e
sung8 <heechuw@gmail.com>
2020-10-10 22:19:45 -0400

    react component delete todo button

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -3,6 +3,18 @@ import React, { Fragment, useState, useEffect } from "react";
 const ListTodos = () => {
   const [todos, setTodos] = useState([]);
 
+  // delete todo
+  async function deleteTodo(id) {
+    try {
+      const res = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: "DELETE",
+      });
+      setTodos(todos.filter((todo) => todo.todo_id !== id));
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
   async function getTodos() {
     const res = await fetch("http://localhost:5000/todos");
     const todoArray = await res.json();
@@ -35,10 +47,17 @@ const ListTodos = () => {
           </tr>
         */}
           {todos.map((todo) => (
-            <tr>
+            <tr key={todo.todo_id}>
               <td>{todo.description}</td>
               <td>Edit</td>
-              <td>Delete</td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteTodo(todo.todo_id)}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
